5c1c5d365561207da434b524b5ebe6c0c77ecb64
Feliom05 <john_ay86@hotmail.com>
2017-11-24 16:05:53 +0100

    Work on progress - Create User

--- Laja/Migrations/201711241257056_User.Designer.cs
+++ Laja/Migrations/201711241257056_User.Designer.cs
@@ -6,14 +6,14 @@ namespace Laja.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class NewDb : IMigrationMetadata
+    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
+    public sealed partial class User : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(NewDb));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(User));
         
         string IMigrationMetadata.Id
         {
-            get { return "201711241050466_NewDb"; }
+            get { return "201711241257056_User"; }
         }
         
         string IMigrationMetadata.Source
