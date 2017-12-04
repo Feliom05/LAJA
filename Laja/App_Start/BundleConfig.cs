d370e84661e1facc753858f743439695f0db3a71
Lars Börlin <lars.borlin@gmail.com>
2017-12-04 08:47:14 +0100

    Cleanup after datpicker experiance

--- Laja/App_Start/BundleConfig.cs
+++ Laja/App_Start/BundleConfig.cs
@@ -20,13 +20,10 @@ namespace Laja
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap-datepicker.js",
-                      "~/Scripts/DatePickerReady.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-datepicker3.css",
                       "~/Content/site.css"));
         }
     }
