cf5f368642236b6bd26b4f2c91ff593f52592e2d
Lars Börlin <lars.borlin@gmail.com>
2017-12-07 17:03:19 +0100

    Secound Iteration AssignmentSubmit

--- Laja/Controllers/AssignmentSubmitController.cs
+++ Laja/Controllers/AssignmentSubmitController.cs
@@ -22,12 +22,18 @@ namespace Laja.Controllers
 
             var moduleId = db.Activities.Find(activityId).ModuleId;
             var courseId = db.Modules.Find(moduleId).CourseId;
+
+           
+
+            //var activityDocs = db.Documents.Where(d => d.ActivityId == activityId);
+
             var course = db.Courses
                 .Where(c => c.Id == courseId)
                 .Include(c => c.Students)
                 .Include(c => c.Documents)
                 .FirstOrDefault();
 
+            //var assignmentDocuments = db.Documents.Where(d => d.ActivityId == activityId);
 
             var assignmentSubmit = new AssignmentSubmitViewModel()
             {
